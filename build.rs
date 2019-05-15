7673ac75121210e6765cd1e1692688de72083466
Dylan McKay <me@dylanmckay.io>
2019-05-15 23:10:06 +1200

    Fix the flonum predicate
    The check needs to be against the size of 'long long', not the size of
    uint64_t.

--- build.rs
+++ build.rs
@@ -17,8 +17,26 @@ fn main() {
 
     if LINK_CRYPT { println!("cargo:rustc-link-lib=dylib=crypt"); }
 
-    if mem::size_of::<libc::uintptr_t>() >= mem::size_of::<f64>() {
+    if should_use_flonum() {
         println!("cargo:rustc-cfg=mri_use_flonum");
     }
+}
+
+/// Logic taken from MRI's `ruby/ruby.h`.
+fn should_use_flonum() -> bool {
+    const SIZEOF_LONG: usize = mem::size_of::<libc::c_long>();
+    const SIZEOF_LONG_LONG: usize = mem::size_of::<libc::c_longlong>();
+    const SIZEOF_VOIDP: usize = mem::size_of::<*const libc::c_void>();
+    const SIZEOF_DOUBLE: usize = mem::size_of::<*const libc::c_double>();
+
+    let sizeof_value = if SIZEOF_LONG == SIZEOF_VOIDP {
+        SIZEOF_LONG
+    } else if SIZEOF_LONG_LONG == SIZEOF_VOIDP {
+        SIZEOF_LONG_LONG
+    } else {
+        panic!("error: ruby requires sizeof(void*) == sizeof(long) or sizeof(LONG_LONG) to be compiled");
+    };
+
+    sizeof_value >= SIZEOF_DOUBLE
 }
 
